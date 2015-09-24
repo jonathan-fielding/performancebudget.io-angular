a2a5c516b4ae1e543845533f201033e8e99c4497
Jonathan Fielding <j.fielding@me.com>
2015-09-24 18:29:02 +0100

    styling changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,17 @@ var app = angular.module('app', []);
 app.controller('BudgetCtrl', function($scope){
 	$scope.loadtime = "";
 	$scope.budget = 0;
-	$scope.connection = 0;
+	$scope.connection = "";
+	$scope.error = "";
 
 	$scope.calculate = function() {
 		var connectionSpeed = parseFloat($scope.connection);
 		var loadTime = parseFloat($scope.loadtime);
+
+		if(isNaN(loadTime) || isNaN(loadTime)) {
+			$scope.budget = 0;
+			$scope.error 
+		}
 
 		$scope.budget = loadTime * connectionSpeed;
 	};
