1bf8b7ee957d6e0d6aa70725b18affd797cf0c03
Jonathan Fielding <j.fielding@me.com>
2015-09-24 21:30:28 +0100

    work on sliders

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,34 @@
 var app = angular.module('app', ['rzModule']);
 
-app.controller('BudgetCtrl', function($scope){
+app.controller('BudgetCtrl', function($scope, $rootScope, $timeout){
 	$scope.loadtime = "";
-	$scope.budget = 0;
+	$scope.budget = 100;
 	$scope.connection = "";
 	$scope.error = "";
 	$scope.step = 1;
 	$scope.cssSlider = 0;
+	$scope.htmlSlider = 0;
+	$scope.jsSlider = 0;
+	$scope.imagesSlider = 0;
+	$scope.videoSlider = 0;
+	$scope.fontsSlider = 0;
 
 	$scope.calculate = function() {
 		var connectionSpeed = parseFloat($scope.connection);
 		var loadTime = parseFloat($scope.loadtime);
 
-		$scope.step = 2;
-
-		if(isNaN(loadTime) || isNaN(loadTime)) {
-			$scope.budget = 0;
-			$scope.error = 'Please enter';
+		if(isNaN(connectionSpeed) || isNaN(loadTime)) {
+			$scope.error = 'Please enter ';
 		}
+		else {
+			$scope.step = 2;
+			$scope.budget = loadTime * connectionSpeed;
 
-		$scope.budget = loadTime * connectionSpeed;
+			$timeout(function(){
+				$rootScope.$broadcast('rzSliderForceRender');
+			}, 10)
+
+			
+		}
 	};
 })
